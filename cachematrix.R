689f6d662536549134d2ae02d53b8e5ee11e7998
anggergr <89477879+anggergr@users.noreply.github.com>
2021-08-25 01:12:53 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,47 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+inv<-NULL
+set<-function(y){
+x<<-y
+inv<<-NULL
+}
+get<-function() {x}
+setInverse<-function(inverse) {inv<<-inverse}
+getInverse<-function() {inv}
+list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+}
+cacheSolve<-function(x,...){
+inv<-x$getInverse()
+if(!is.null(inv)){
+message("getting cached data")
+return(inv)
+}
+mat<-x$get()
+inv<-solve(mat,...)
+x$setInverse(inv)
+inv
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+source("Caching the Inverse of a Matrix.R")
+a<-makeCacheMatrix(matrix(c(23,12,19,98),nrow=2,ncol=2))
+a$get()
+[,1] [,2]
+[1,] 23 19
+[2,] 12 98
+a$getInverse()
+NULL
+cacheSolve(a)
+[,1] [,2]
+[1,] 0.048371175 -0.009378085
+[2,] -0.005923001 0.011352419
+cacheSolve(a)
+getting cached data
+[,1] [,2]
+[1,] 0.048371175 -0.009378085
+[2,] -0.005923001 0.011352419
+a$getInverse()
+[,1] [,2]
+[1,] 0.048371175 -0.009378085
+[2,] -0.005923001 0.011352419
