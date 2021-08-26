5ec77060b98fa53451757153da06a35409355202
anggergr <89477879+anggergr@users.noreply.github.com>
2021-08-26 12:25:36 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 ## This Function will make a cache matrix and set inv as null
 makeCacheMatrix<-function(x=matrix()){
-#assume that matrix is invertible
-      inv<-NULL
-#setting the value of the matrix
-      set<-function(y){                                                 
-      x<<-y
-      inv<<-NULL
-}
-      get<-function() {x}
-      setInverse<-function(inverse) {inv<<-inverse}
-      getInverse<-function() {inv}
-# Creates list of functions
-      list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)  
+  #assume that matrix is invertible
+  inv<-NULL
+  #setting the value of the matrix
+  set<-function(y){
+        x<<-y
+        inv<<-NULL
+  }
+  get<-function() {x}
+  setInverse<-function(inverse) {inv<<-inverse}
+  getInverse<-function() {inv}
+  # Creates list of functions
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 ## This functionn is cacheSolve and returns the matrix to inverse
-cacheSolve<-function(x,...){                                       '
-## Return a matrix that is the inverse of 'x
-      inv<-x$getInverse()
-      if(!is.null(inv)){
-      message("getting cached data")
-      return(inv)
-}
-      mat<-x$get()
-      inv<-solve(mat,...)
-      x$setInverse(inv)
-      inv
+cacheSolve<-function(x,...){
+  ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
 
 # Test the function
